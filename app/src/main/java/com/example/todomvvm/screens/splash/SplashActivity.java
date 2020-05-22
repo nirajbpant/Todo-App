5ddb78f0a4d49986fccb55dc6b6c1f8eb5071313
Niraj Pant <nirajbpant@gmail.com>
2020-05-22 21:43:02 +0545

    added splash activity

---
+++ app/src/main/java/com/example/todomvvm/screens/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.todomvvm.screens.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.todomvvm.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
